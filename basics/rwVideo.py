8f6e5708140c81eac496ccc32e8fd3dffea47f2e
MrCode97 <robin.seidel@epost.ch>
2017-01-23 15:35:42 +0100

    capture and save, but error on gstreamer

--- basics/rwVideo.py
+++ basics/rwVideo.py
@@ -4,7 +4,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('capRecord.avi',fourcc, 5, (320, 240))
+out = cv2.VideoWriter('capRecord.avi',fourcc, 5, (640, 480))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
