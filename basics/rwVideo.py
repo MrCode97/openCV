d017781857d1e87263e3ec463b447470580acc8a
MrCode97 <robin.seidel@epost.ch>
2017-01-23 15:26:08 +0100

    capture and save, but error on gstreamer

--- basics/rwVideo.py
+++ basics/rwVideo.py
@@ -3,11 +3,19 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('capRecord.avi',fourcc, 5, (320, 240))
+
+while(cap.isOpened()):
     ret, frame = cap.read()
-    cv2.imshow('CameraCapture', frame)
-    if cv2.waitKey(1) & 0xFF == 27:     # 27 -> ESC-key
+    if ret == True:
+        out.write(frame)
+        cv2.imshow('CameraCapture', frame)
+        if cv2.waitKey(1) & 0xFF == 27:     # 27 -> ESC-key
+            break
+    else:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
