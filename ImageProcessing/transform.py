db8c095bd1618fc225f7b6c29c3da45614797d49
MrCode97 <robin.seidel@epost.ch>
2017-02-07 13:22:24 +0100

    2 way how to transfoem img -formated

--- ImageProcessing/transform.py
+++ ImageProcessing/transform.py
@@ -3,11 +3,11 @@ import numpy as np
 img = cv2.imread('chess1.jpg')
 
 # resize-way1
-way1 = cv2.resize(img,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+way1 = cv2.resize(img, None, fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
 
 # resize-way2
 height, width = img.shape[:2]
-way2 = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+way2 = cv2.resize(img, (2*width, 2*height), interpolation = cv2.INTER_CUBIC)
 
 
 # display and destroy
