ee9c6a0ecae9a7ac04a277c0cd0ef9bd16a4839f
MrCode97 <robin.seidel@epost.ch>
2017-02-14 20:37:41 +0100

    commented

--- ImageProcessing/ImageSmoothing/ImgBluring.py
+++ ImageProcessing/ImageSmoothing/ImgBluring.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('original.jpg')
+# load Img
+img = cv2.imread('original.png')
 
-ImgBLUR = cv2.blur(img, (5, 5))
+# create new blured Img
+ImgBLUR = cv2.blur(img, (5, 5))     # (5, 5) == kernel size
 
+# show and close
+cv2.imshow('original.png', img)
 cv2.imshow('ImgBlured', ImgBLUR)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- ImageProcessing/ImageSmoothing/ImgMedianBluring.py
+++ ImageProcessing/ImageSmoothing/ImgMedianBluring.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('original.jpg')
+# load Img
+img = cv2.imread('original.png')
 
-ImgMedianBlur = cv2.medianBlur(img, 5)
+# create new medianBlured Img
+ImgMedianBlur = cv2.medianBlur(img, 5)  # 5 is kernel size (the higher the more BLUR)
 
+# show and close 
+cv2.imshow('original.jpg', img)
 cv2.imshow('ImgMedianBlured', ImgMedianBlur)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
