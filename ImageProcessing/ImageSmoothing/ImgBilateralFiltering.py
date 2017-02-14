7354d32d0275d3f2c241d6529d9a6120a7d7f59e
MrCode97 <robin.seidel@epost.ch>
2017-02-14 20:28:48 +0100

    updated for new picture

--- ImageProcessing/ImageSmoothing/ImgBilateralFiltering.py
+++ ImageProcessing/ImageSmoothing/ImgBilateralFiltering.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('original.jpg')
+img = cv2.imread('wood.jpg')
 
 ImgBilateralFiltered = cv2.bilateralFilter(img, 9, 75, 75)
 
+cv2.imshow('wood.jpg', img)
 cv2.imshow('ImgBilateralFiltered', ImgBilateralFiltered)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
