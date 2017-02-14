e3115e7cf038d248200a9c6968fc81c040612545
MrCode97 <robin.seidel@epost.ch>
2017-02-14 20:42:48 +0100

    commented

--- ImageProcessing/ImageSmoothing/ImgBilateralFiltering.py
+++ ImageProcessing/ImageSmoothing/ImgBilateralFiltering.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
 
+# load img
 img = cv2.imread('wood.jpg')
 
-ImgBilateralFiltered = cv2.bilateralFilter(img, 9, 75, 75)
-
+# create new bilateralfiltered picture from original ('img')
+ImgBilateralFiltered = cv2.bilateralFilter(img, 9, 75, 75)      # 9 -> param d
+                                                                # 75 -> sigmaColor
+                                                                # 75 -> sigmaSpace
+# show imgs and close 
 cv2.imshow('wood.jpg', img)
 cv2.imshow('ImgBilateralFiltered', ImgBilateralFiltered)
 cv2.waitKey(0)
