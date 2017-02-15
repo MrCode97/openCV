31eff475eea105f5d776dee61b323d097b24dd5b
MrCode97 <robin.seidel@epost.ch>
2017-02-15 15:41:07 +0100

    comments added

--- ImageProcessing/ImageGradient/gradient.py
+++ ImageProcessing/ImageGradient/gradient.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
 
+# load img
 img = cv2.imread('original.jpg',0)
 
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
+# apply Laplacian function
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
 
-sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+# apply Sobel function
+sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)  # (1, 0) -> order of the derivative: (dx, dy)
+sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=7)  # kSize -> kernelSize: sh ould be 1, 3, 5, 7
 
+
+# show imgs and close
 cv2.imshow('original', img)
 cv2.imshow('sobel-X', sobelx)
 cv2.imshow('sobel-Y', sobely)
