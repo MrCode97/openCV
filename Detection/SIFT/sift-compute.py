d2d47105ab990f03336e5672bfbbedecd54b19a3
MrCode97 <robin.seidel@epost.ch>
2017-02-21 16:09:59 +0100

    HowTo compute descriptors

---
+++ Detection/SIFT/sift-compute.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+skyline = cv2.imread('skyline.jpg')
+skylineGRAY = cv2.imread('skyline.jpg', 0)
+
+# Option 1
+#
+# create sift object
+sift = cv2.xfeatures2d.SIFT_create()
+# get / detect KeyPoints on 'skylineGRAY'
+keyPoints = sift.detect(skylineGRAY, None)
+# computes descriptors from keypoint
+ComputeKP1, des1 = sift.compute(skylineGRAY, keyPoints)   # ComputeKP gives us a list uf found keyPoints
+# des is a numpy array of shape \(Number\_of\_Keypoints \times 128\)
+# print des
+
+
+# Option 2
+# 
+sift2 = cv2.xfeatures2d.SIFT_create()
+ComputeKP2, des2 = sift.detectAndCompute(skylineGRAY, None)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
