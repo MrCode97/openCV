f7442dc1e6147ba4672d3fc327ceee5b5882c889
MrCode97 <robin.seidel@epost.ch>
2017-02-21 15:49:55 +0100

    size and orientation of KeyPoints added

--- Detection/SIFT/sift.py
+++ Detection/SIFT/sift.py
@@ -17,7 +17,14 @@ keyPoints = sift.detect(skylineGRAY, None)
 # create new img where the keyPoints are drawn on the coloredSkyline
 skylineKeyPoints = cv2.drawKeypoints(skyline, keyPoints, skylineGRAY)
 
+# adds the size and th e orientation of the KeyPoints
+skylineKeyPointsSizeOrientation = cv2.drawKeypoints(skyline, keyPoints, skylineGRAY, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+cv2.imshow('sift_keypoints.jpg',skylineKeyPointsSizeOrientation)
+cv2.imwrite('skylineKeyPointsSizeOrientation.jpg', skylineKeyPointsSizeOrientation)
+
 cv2.imshow('Skyline with KeyPoints', skylineKeyPoints)
 cv2.imwrite('SkylineKeyPoints.jpg', skylineKeyPoints)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
