da02c45f5c7171072839dadf70102cf2cd3f5e65
MrCode97 <robin.seidel@epost.ch>
2017-02-21 14:48:19 +0100

    HowTo get and display keypoints

---
+++ Detection/SIFT/sift.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+skyline = cv2.imread('skyline.jpg')
+skylineGRAY = cv2.imread('skyline.jpg', 0)
+
+# create sift object
+sift = cv2.xfeatures2d.SIFT_create()
+
+# get / detect KeyPoints on 'skylineGRAY'
+keyPoints = sift.detect(skylineGRAY, None)
+# sift.detect() function finds the keypoint in the images. You can pass a mask if you want to search only a part of image. 
+# Each keypoint is a special structure which has many attributes like its (x,y) coordinates, size of the meaningful neighbourhood, 
+# angle which specifies its orientation, response that specifies strength of keypoints etc.
+# src: docs.opencv.org/
+
+# create new img where the keyPoints are drawn on the coloredSkyline 
+skylineKeyPoints = cv2.drawKeypoints(skylineGRAY, keyPoints, skyline)
+
+cv2.imshow('Skyline with KeyPoints', skylineKeyPoints)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
