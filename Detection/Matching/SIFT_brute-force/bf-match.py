caf3199d6a1378ebde327c6aab85b5880370f36d
MrCode97 <robin.seidel@epost.ch>
2017-02-23 15:51:08 +0100

    more comments

--- Detection/Matching/SIFT_brute-force/bf-match.py
+++ Detection/Matching/SIFT_brute-force/bf-match.py
@@ -1,27 +1,26 @@
 import numpy as np
 import cv2
 
-searchImg = cv2.imread('cover.jpg', 0)          # queryImage
-container = cv2.imread('cont1.jpg', 0) # trainImage
-
+searchImg = cv2.imread('skyline.jpg', 0)        # Picture that we are looking for
+container = cv2.imread('cont2.jpg', 0)          # Picture which contains the picture we are looking for
 # create SIFT
 sift = cv2.xfeatures2d.SIFT_create()
 
-# find the keypoints and descriptors with SIFT
+# get keypoints, descriptors with SIFT
 keyPoints_search, des_search = sift.detectAndCompute(searchImg, None)
 keyPoints_container, des_container = sift.detectAndCompute(container, None)
 
-# BFMatcher with default params
+# create BFMatcher (default params)
 bf = cv2.BFMatcher()
 matches = bf.knnMatch(des_search,des_container, k=2)
 
-# Apply ratio test
+# ratio test
 good = []
 for m, n in matches:
     if m.distance < 0.75*n.distance:
         good.append([m])
 
 # cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(searchImg, keyPoints_search, container, keyPoints_container, good, None, flags=2)
+result = cv2.drawMatchesKnn(searchImg, keyPoints_search, container, keyPoints_container, good, None, flags=2) # Note although it says that 'outImg' is optional I had to fill it with 'None'
 
-cv2.imwrite('SIFT-matching.jpg', img3)
+cv2.imwrite('bf-matchSIFT-pic.jpg', result)
