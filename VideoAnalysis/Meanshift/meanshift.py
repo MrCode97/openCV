ccb15845c3948cb63c97bd70a159a18aa85ffd46
MrCode97 <robin.seidel@epost.ch>
2017-02-27 18:38:47 +0100

    explained

--- VideoAnalysis/Meanshift/meanshift.py
+++ VideoAnalysis/Meanshift/meanshift.py
@@ -31,20 +31,20 @@ while(1):                                                                       
     ret ,frame = cap.read()
     if ret == True:                                                             # check if there is still incoming frame
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)                            # get and convert frame in hsv
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
 
         # apply meanshift to get the new location
         ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
         # Draw it on image
-        x,y,w,h = track_window
-        img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+        x,y,w,h = track_window                                                  # get coordinates from track_window
+        img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255, 2)                   # draw rectangle from track_window-coordinates
         cv2.imshow('img2',img2)
-        k = cv2.waitKey(60) & 0xff
+        k = cv2.waitKey(60) & 0xff                                              # listen and store key
         if k == 27:
             break
         else:
-            cv2.imwrite(chr(k)+".jpg",img2)
+            cv2.imwrite(chr(k)+".jpg",img2)                                     # write img with name from stored key
     else:
         break
 cv2.destroyAllWindows()
